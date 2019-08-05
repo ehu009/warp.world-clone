c3ffc682775b323f71f03ecb0c7c628037da1a27
ehu009 <jenssen.eirik@gmail.com>
2019-08-05 06:48:51 -0400

    do not log to stdout

--- web.sh
+++ web.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
+
 iptables -t nat -A OUTPUT -o lo -p tcp --dport 80 -j REDIRECT --to-port 3000
+
 su - rails <<!
 cd /home/eirik/warp.world-clone
-whoami
-rails s -b 0.0.0.0
 
-echo "fire on my penis"
+RAILS_ENV=production
+bundle exec rake assets:precompile
+#RAILS_LOG_TO_STDOUT=yes
+rails s -b 0.0.0.0 -e production
+
 exit
 !
